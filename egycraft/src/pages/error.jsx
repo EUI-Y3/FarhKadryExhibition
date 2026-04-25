9d06356434cf84332865c17f43258f5429104bbf
Farh Kadry <23-101289@students.eui.edu.eg>
2026-04-26 01:50:24 +0300

    error structure added

--- egycraft/src/pages/error.jsx
+++ egycraft/src/pages/error.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import errormsg from './../assets/fillerIllus02.svg'
+import decor from './../assets/pagedecor.svg'
+import './error.css'
+import HeadingBlock from '../components/common/heading';
+
 const Error = () => {
     return ( <>
-    
+    <div className="secContainer">
+        <section className='section2'>
+            <img src={errormsg} alt="" />
+            <img className='pageDecor float' src={decor} alt="" />
+            <div>
+                <HeadingBlock 
+                heading="Opps!"
+                subheading="sklklskl"
+                style1="dark left"
+                />
+            </div>
+        </section>
+    </div>
     </> );
 }
  
