5ba16c611720bacf8dc4bea7b811636b64cf4529
Farh Kadry <23-101289@students.eui.edu.eg>
2026-05-02 01:17:23 +0300

    created burger menu

--- egycraft/src/components/layout/header.jsx
+++ egycraft/src/components/layout/header.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 import './header.css'
 import Navigation from './nav';
 import logo1 from './../../assets/logoheader.svg'
 import headerbg from './../../assets/headerleft.png'
 import headerbg2 from './../../assets/headerright.png'
 import Button from '../common/button';
+import burger from './../../assets/hiwIcon01.svg'
+import close from './../../assets/hiwIcon01.svg'
+import './../layout/popup.css'
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const openPopup = () => setIsOpen(true);
+    const closePopup = () => setIsOpen(false);
+
     return ( <>
     <div className="headerCont">
         <header>
             <img className='headerBg' src={headerbg2} alt="" />
             <img className='headerBg headerBg2' src={headerbg} alt="" />
         <img className='logo' src={logo1} alt="fatima hand logo" />
-        <Navigation />
+        <div className="navCont2">
+            <Navigation />
+        </div>
         <Button style1="size1 blue" cta="Book my ticket" link="/booking" />
+        <button className="btn3 burgermenu" onClick={openPopup}>
+                            <img src={burger} alt="menu" />
+                        </button>
     </header>
     </div>
+     {/* MOBILE POPUP */}
+            <div className={`popUp ${isOpen ? "show" : "hide"}`}>
+                <div>
+                    <ul className="burgernav">
+                        <Navigation />
+                    </ul>
+
+                    {/* CLOSE BUTTON */}
+                    <button className="btn3 closed" onClick={closePopup}>
+                        <img src={close} alt="close" />
+                    </button>
+                </div>
+            </div>
     </> );
 }
  
